73294202f5513a0119da1be472eff1240af1a7b1
desaidhruv <dhruv.desai@kevit.io>
2023-05-25 19:32:58 +0530

    project is complete

--- e-commerce/src/Component/SubComponent/Specific Component/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "../SpecificComponentCSS/Button.module.css";
-const Button = (props) => {
-  return (
-    <button className={styles.commonbutton} type={props.type}>
-      {props.content}
-    </button>
-  );
-};
-export default Button;
